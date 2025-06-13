3793468711284d6e3646a6ba98c6852f1f84cbcb
Ivo Popov <ivopopv@gmail.com>
2025-06-13 14:41:02 +0300

    Added Engine Interface and Engine impl, still cannot compile because dependencies are not implemented

---
+++ src/Startup.java
@@ -0,0 +1,9 @@
+import core.LogisticEngineImpl;
+
+public class Startup {
+    public static void main(String[] args) {
+        LogisticEngineImpl engine = new LogisticEngineImpl();
+        engine.start();
+
+    }
+}
